98f2fb24c1e416106ae93f1de744ea375faaf97f
nicole <garance.loison@gmail.com>
2018-03-09 14:12:13 -0700

    two files instead of 1

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var http = require('http'),
+fs = require('fs'),
+io = require('socket.io'),
+index;
+fs.readFile('./index.html', function (err, data) {
+            if (err) {
+            throw err;
+            }
+            index = data;
+            });
+var server = http.createServer(function(request, response) {
+                               response.writeHeader(200, {"Content-Type": "text/html"});
+                               response.write(index);
+                               response.end();
+                               }).listen(3000);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+var socket = io.listen(server);
